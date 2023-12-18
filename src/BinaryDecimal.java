1f50a3b8216956275dfd041e19b87aaa71a18446
Elena Metelli <elena.metelli@itsrizzoli.it>
2023-12-18 11:33:02 +0100

    Sviluppo funzione di conversione

--- src/BinaryDecimal.java
+++ src/BinaryDecimal.java
@@ -2,4 +2,15 @@ public class BinaryDecimal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+public static int binaryDecimal(String binaryString) {
+    int decimalValue = 0;
+    int len = binaryString.length();
+    for (int i=0; i<len; i++){
+        int digit = Character.getNumericValue(binaryString.charAt(i));
+        int power = len - i - 1;
+        decimalValue = decimalValue + (int) (digit*Math.pow(2, power));
+    }
+    return decimalValue;
+}
+
 }
