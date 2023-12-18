8d534ad5b1b05eaf30e8602352f5461e78ac313c
Elena Metelli <elena.metelli@itsrizzoli.it>
2023-12-18 10:54:35 +0100

    input utente

--- src/BinaryDecimal.java
+++ src/BinaryDecimal.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class BinaryDecimal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci una stringa binaria:");
+        String binaryString = scanner.nextLine();
+        int decimalValue = 0;
+        //int decimalValue = BinaryDecimal(binaryString);
+        System.out.println("Valore decimale:" + decimalValue);
+        scanner.close();
     }
 }
